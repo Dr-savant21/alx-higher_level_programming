38f271e7333656b7d60c1c8290ba9d458167c75b
Dr-savant21 <israelnosakhare21@gmail.com>
2023-05-13 00:51:03 +0100

    few changes made to 0x012

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,9 @@
 /*
   Write a script that prints two arguments passed to it still using the
   concept of process.argv
+
   Author: sammykingx
+
 const firstArg = process.argv[2], secondArg = process.argv[3];
 if (firstArg === undefined || secondArg === undefined) {
   console.log(firstArg + ' is ' + secondArg);
